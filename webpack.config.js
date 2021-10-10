c87fdb5a384055b34e0479caad0231345730ec37
ANNA8020 <limJE2005@gmail.com>
2021-10-10 16:53:45 +0900

    loader 합치기?

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,17 @@ module.exports = {
         rules: [
             {
                 test: /\.js$/,
+                // 모든 js file을 가져다가 몇가지 변환 시킨다는 뜻
                 use: {
                     loader: 'babel-loader',
                      options: {
                      presets: [['@babel/preset-env', { targets: "defaults" }]],
                     },
                 },
+            },
+            {
+                test: /\.scss$/,
+                use: ["style-loader", "css-loader", "sass-loader"],
             },
         ],
     },
