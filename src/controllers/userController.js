8fefc8a16b83b23cb759f7889cd172bb3b9f8e20
ANNA8020 <limJE2005@gmail.com>
2021-11-04 12:12:33 +0900

    input에 placeholder 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,18 @@
+import User from "../models/User";
+
 export const getJoin = (req, res) => res.render("join", { pageTitle: "Join" });
-export const postJoin = (req, res) => {
-    console.log(req.body);
-    res.end();
+export const postJoin = async(req, res) => {
+    // create가 끝나는 걸 기달려줘야 해서 async 씀
+    const { name, username, email, password, location } = req.body;
+    await User.create({
+        name, 
+        username, 
+        email, 
+        password, 
+        location
+        // 데이터 전달⤴
+    })
+    return res.redirect("/login");
 };
 export const edit = (req, res) => res.send("Edit User");
 export const remove = (req, res) => res.send("Remove User");
